2df00548161d3358a792c2786ba5fa0124ff2919
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-03 11:30:38 +0200

    Conduct normalizing before feature hashing
    Applying feature scaling before feature hashing ensures that
    the numerical features are properly scaled and helps maintain
    the relative importance of the features during hashing.
    It can also prevent issues such as numerical features dominating
    the hashed representation.

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 import pandas as pd
 from sklearn.feature_extraction import FeatureHasher
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 input_directory_path = Path(input('Input directory: '))
 output_file_path = input('Output directory: ') + '/' + 'fsae_mi_2013-2017'
@@ -18,6 +18,13 @@ res_df = pd.concat(
 res_df.dropna(inplace=True)
 res_df.drop(columns='Car Num', inplace=True)
 
+# temporarily drop the Team column for normalizing, while preserving it
+# for feature hashing
+tmp_df = res_df.drop(columns='Team')
+
+scaler = MinMaxScaler()
+res_df[tmp_df.columns] = scaler.fit_transform(tmp_df)
+
 hasher = FeatureHasher(n_features=40, input_type='string')
 hashed_array = hasher.fit_transform(
     [[team] for team in res_df['Team']]).toarray()
@@ -31,9 +38,6 @@ hashed_data = pd.DataFrame(
 
 res_df = res_df.join(hashed_data)
 res_df.drop(columns='Team', inplace=True)
-
-scaler = MinMaxScaler()
-res_df[res_df.columns] = scaler.fit_transform(res_df[res_df.columns])
 
 res_df_train, res_df_test = train_test_split(res_df)
 
