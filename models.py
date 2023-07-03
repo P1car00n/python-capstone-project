e38c34506d407fde280c941edb2a48ca0f5ccdb9
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-03 15:27:20 +0200

    Create linear regression class

---
+++ models.py
@@ -0,0 +1,24 @@
+from sklearn.linear_model import LinearRegression
+
+
+class Model:
+
+    def __init__(self, description, model):
+        self.description = description
+        self.model = model
+
+    def get_prediction(self, samples):
+        return self.model.predict(samples)
+
+    def get_score(self, X, y):
+        return self.model.score(X, y)
+
+    def __repr__(self) -> str:
+        return self.description
+
+
+class LRM(Model):
+
+    def __init__(self, X, y, description='Linear regression model', **kwargs):
+        Model.__init__(self, description,
+                       model=LinearRegression(**kwargs).fit(X, y))
