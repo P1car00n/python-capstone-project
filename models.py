5c2662233735661c5041a9efc503c128e880e951
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-04 15:17:02 +0200

    Implement gradient boosting regression

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from sklearn.linear_model import LinearRegression, Ridge
 
 
@@ -45,6 +45,18 @@ class RFR(Model):
                        model=RandomForestRegressor(**kwargs).fit(X, y))
 
 
+class GBR(Model):
+
+    def __init__(
+            self,
+            X,
+            y,
+            description='gradient boosting regression model',
+            **kwargs):
+        Model.__init__(self, description,
+                       model=GradientBoostingRegressor(**kwargs).fit(X, y))
+
+
 if __name__ == '__main__':
     # getting the data
     dataset_train = pd.read_parquet(
@@ -80,3 +92,18 @@ if __name__ == '__main__':
     y_pred_rfr = rfr.get_prediction(X_test)
     print(rfr.get_score(X_train, y_train))
     print(rfr.get_score(X_test, y_test))
+
+    # gradient boosting
+    gbr = GBR(
+        X_train,
+        y_train,
+        learning_rate=0.01,
+        max_depth=3,
+        max_features=None,
+        min_samples_leaf=2,
+        min_samples_split=10,
+        n_estimators=500,
+        subsample=0.8)
+    y_pred_gbr = gbr.get_prediction(X_test)
+    print(gbr.get_score(X_train, y_train))
+    print(gbr.get_score(X_test, y_test))
